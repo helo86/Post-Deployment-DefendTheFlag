116dc0e0ed182fa753e2dfa15e973639f5f11ce0
Pentest Factory GmbH <63093362+pentestfactory@users.noreply.github.com>
2020-08-28 11:56:56 +0200

    Update configure-ou.ps1

--- configure-ou.ps1
+++ configure-ou.ps1
@@ -126,8 +126,3 @@ else
 {
   Write-Host "'[{0:HH:mm}]' -f (Get-Date)) ServiceAccounts OU already exists. Moving On."
 }
-
-# Sysprep breaks auto-login. Let's restore it here:
-Set-ItemProperty "HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Winlogon" -Name AutoAdminLogon -Value 1
-Set-ItemProperty "HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Winlogon" -Name DefaultUserName -Value "vagrant"
-Set-ItemProperty "HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Winlogon" -Name DefaultPassword -Value "vagrant"
